a492d23cda5af1ba49167fca220cceb2b9fba400
gallegosjennifer <jgallegos@myrobal.com>
2023-03-30 11:24:17 -0600

    Add products to car

--- Shopping/Data/DataContext.cs
+++ Shopping/Data/DataContext.cs
@@ -12,12 +12,15 @@ namespace Shopping.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Country> Countries { get; set; }
         public DbSet<City> Cities { get; set; }
-        public DbSet<State> States { get; set; }
+   
         public DbSet<Product> Products { get; set; }
 
         public DbSet<ProductCategory> ProductCategories { get; set; }
 
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<TemporalSale> TemporalSales { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
