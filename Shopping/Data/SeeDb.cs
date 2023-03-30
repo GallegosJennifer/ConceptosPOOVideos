e831258cb6fcaae073dfcbfca1e7fef3461ff638
gallegosjennifer <jgallegos@myrobal.com>
2023-03-30 10:09:55 -0600

    Finisih Product CRUD

--- Shopping/Data/SeeDb.cs
+++ Shopping/Data/SeeDb.cs
@@ -1,4 +1,5 @@
-﻿using Shopping.Data.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
 using Shopping.Enums;
 using Shopping.Helpers;
 
@@ -8,11 +9,13 @@ namespace Shopping.Data
     {
         private readonly DataContext _context;
         private readonly IUserHelper _userHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public SeeDb(DataContext context, IUserHelper userHelper)
+        public SeeDb(DataContext context, IUserHelper userHelper, IBlobHelper blobHelper)
         {
             _context = context;
             _userHelper = userHelper;
+            _blobHelper = blobHelper;
         }
         public async Task SeeAsync()
         {
@@ -21,11 +24,71 @@ namespace Shopping.Data
             await CkeckCategoriesAsync();
             await CheckCountriesAsync();
             await CheckRolesAsync();
-            await CheckUserAsync("1010", "Juan", "Zuluaga", "zulu@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", UserType.Admin);
-            await CheckUserAsync("2020", "Juan", "Jennifer", "jeni@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", UserType.User);
+            await CheckUserAsync("1010", "Juan", "Zuluaga", "zulu@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "tony.jpg", UserType.Admin);
+            await CheckUserAsync("2020", "Katy", "Perry", "katy@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "katy.jpg", UserType.User);
+            await CheckUserAsync("3030", "Rami", "Malek", "rami@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "rami.jpg", UserType.User);
+            await CheckUserAsync("4040", "Angelina", "Jolie", "angelina@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "Angelina.jpg", UserType.User);
+            await CheckUserAsync("5050", "Zendaya", "Rios", "zendaya@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "zendaYA.jpg", UserType.User);
+            await CheckProductsAsync();
 
         }
 
+        private async Task CheckProductsAsync()
+        {
+            if (!_context.Products.Any())
+            {
+                await AddProductAsync("Adidas Superstar", 270000M, 12F, new List<string>() { "Calzado", "Deportes" }, new List<string>() { "adidasbkancos.jpg" });
+                await AddProductAsync("Nike For One", 250000M, 12F, new List<string>() { "Calzado", "Deportes" }, new List<string>() { "naik.jpg" });
+                await AddProductAsync("AirPods", 1300000M, 12F, new List<string>() { "Tecnología", "Apple" }, new List<string>() { "airpos.jpg" });
+                await AddProductAsync("Audifonos Bose", 870000M, 12F, new List<string>() { "Tecnología" }, new List<string>() { "bose.jpg" });
+                await AddProductAsync("Bicicleta Ribble", 12000000M, 6F, new List<string>() { "Deportes" }, new List<string>() { "bici.jpg" });
+                await AddProductAsync("Camisa Cuadros", 56000M, 24F, new List<string>() { "Ropa" }, new List<string>() { "cuadros.jpg" });
+                await AddProductAsync("Casco Bicicleta", 820000M, 12F, new List<string>() { "Deportes" }, new List<string>() { "casco.jpg" });
+                //await AddProductAsync("iPad", 2300000M, 6F, new List<string>() { "Tecnología", "Apple" }, new List<string>() { "ipad.jpg" });
+                await AddProductAsync("iPhone 13", 5200000M, 6F, new List<string>() { "Tecnología", "Apple" }, new List<string>() { "iphone.jpg" });
+                await AddProductAsync("Mac Book Pro", 12100000M, 6F, new List<string>() { "Tecnología", "Apple" }, new List<string>() { "macbook.jpg" });
+                await AddProductAsync("Mancuernas", 370000M, 12F, new List<string>() { "Deportes" }, new List<string>() { "mancuernas.jpg" });
+                await AddProductAsync("Mascarilla Cara", 26000M, 100F, new List<string>() { "Belleza" }, new List<string>() { "mascarilla.jpg" });
+                await AddProductAsync("New Balance 530", 180000M, 12F, new List<string>() { "Calzado", "Deportes" }, new List<string>() { "balancera.jpg" });
+                await AddProductAsync("New Balance 565", 179000M, 12F, new List<string>() { "Calzado", "Deportes" }, new List<string>() { "balance.jpg" });
+                await AddProductAsync("Nike Zoom", 249900M, 12F, new List<string>() { "Calzado", "Deportes" }, new List<string>() { "zoom.jpg" });
+                //await AddProductAsync("Buso Adidas Mujer", 134000M, 12F, new List<string>() { "Ropa", "Deportes" }, new List<string>() { "buso_adidas.jpg" });
+                //await AddProductAsync("Suplemento Boots Original", 15600M, 12F, new List<string>() { "Nutrición" }, new List<string>() { "Boost_Original.jpg" });
+                await AddProductAsync("Whey Protein", 252000M, 12F, new List<string>() { "Nutrición" }, new List<string>() { "suplemento.jpg" });
+                await AddProductAsync("Arnes Mascota", 25000M, 12F, new List<string>() { "Mascotas" }, new List<string>() { "arnes.jpg" });
+                await AddProductAsync("Cama Mascota", 99000M, 12F, new List<string>() { "Mascotas" }, new List<string>() { "mascotas.jpg" });
+                await AddProductAsync("Silla Gamer", 980000M, 12F, new List<string>() { "Gamer", "Tecnología" }, new List<string>() { "sillagamer.jpg" });
+                await AddProductAsync("Mouse Gamer", 132000M, 12F, new List<string>() { "Gamer", "Tecnología" }, new List<string>() { "mousegamer.jpg" });
+                await _context.SaveChangesAsync();
+            }
+
+        }
+        private async Task AddProductAsync(string name, decimal price, float stock, List<string> categories, List<string> images)
+        {
+            Product prodcut = new()
+            {
+                Description = name,
+                Name = name,
+                Price = price,
+                Stock = stock,
+                ProductCategories = new List<ProductCategory>(),
+                ProductImages = new List<ProductImage>()
+            };
+
+            foreach (string? category in categories)
+            {
+                prodcut.ProductCategories.Add(new ProductCategory { Category = await _context.Categories.FirstOrDefaultAsync(c => c.Name == category) });
+            }
+
+
+            foreach (string? image in images)
+            {
+                Guid imageId = await _blobHelper.UploadBlobAsync($"{Environment.CurrentDirectory}\\wwwroot\\images\\products\\{image}", "products");
+                prodcut.ProductImages.Add(new ProductImage { ImageId = imageId });
+            }
+
+            _context.Products.Add(prodcut);
+        }
 
 
         private async Task<User> CheckUserAsync(
@@ -35,11 +98,13 @@ namespace Shopping.Data
     string email,
     string phone,
     string address,
+     string image,
     UserType userType)
         {
             User user = await _userHelper.GetUserAsync(email);
             if (user == null)
             {
+                Guid imageId = await _blobHelper.UploadBlobAsync($"{Environment.CurrentDirectory}\\wwwroot\\images\\users\\{image}", "users");
                 user = new User
                 {
                     FirstName = firstName,
@@ -51,6 +116,7 @@ namespace Shopping.Data
                     Document = document,
                     City = _context.Cities.FirstOrDefault(),
                     UserType = userType,
+                    ImageId = imageId
                 };
 
                 await _userHelper.AddUserAsync(user, "123456");
@@ -85,6 +151,7 @@ namespace Shopping.Data
             await _context.SaveChangesAsync();
         }
 
+
         private async Task CheckCountriesAsync()
         {
             if (!_context.Countries.Any())
@@ -93,64 +160,128 @@ namespace Shopping.Data
                 {
                     Name = "Colombia",
                     States = new List<State>()
-                    {
-                        new State()
-                        {
-                            Name = "Antioquia",
-                            Cities = new List<City>() {
-                                new City() { Name = "Medellín" },
-                                new City() { Name = "Itagüí" },
-                                new City() { Name = "Envigado" },
-                                new City() { Name = "Bello" },
-                                new City() { Name = "Rionegro" },
-                            }
-                        },
-                        new State()
-                        {
-                            Name = "Bogotá",
-                            Cities = new List<City>() {
-                                new City() { Name = "Usaquen" },
-                                new City() { Name = "Champinero" },
-                                new City() { Name = "Santa fe" },
-                                new City() { Name = "Useme" },
-                                new City() { Name = "Bosa" },
-                            }
-                        },
+            {
+                new State()
+                {
+                    Name = "Antioquia",
+                    Cities = new List<City>() {
+                        new City() { Name = "Medellín" },
+                        new City() { Name = "Itagüí" },
+                        new City() { Name = "Envigado" },
+                        new City() { Name = "Bello" },
+                        new City() { Name = "Sabaneta" },
+                        new City() { Name = "La Ceja" },
+                        new City() { Name = "La Union" },
+                        new City() { Name = "La Estrella" },
+                        new City() { Name = "Copacabana" },
                     }
+                },
+                new State()
+                {
+                    Name = "Bogotá",
+                    Cities = new List<City>() {
+                        new City() { Name = "Usaquen" },
+                        new City() { Name = "Champinero" },
+                        new City() { Name = "Santa fe" },
+                        new City() { Name = "Usme" },
+                        new City() { Name = "Bosa" },
+                    }
+                },
+                new State()
+                {
+                    Name = "Valle",
+                    Cities = new List<City>() {
+                        new City() { Name = "Calí" },
+                        new City() { Name = "Jumbo" },
+                        new City() { Name = "Jamundí" },
+                        new City() { Name = "Chipichape" },
+                        new City() { Name = "Buenaventura" },
+                        new City() { Name = "Cartago" },
+                        new City() { Name = "Buga" },
+                        new City() { Name = "Palmira" },
+                    }
+                },
+                new State()
+                {
+                    Name = "Santander",
+                    Cities = new List<City>() {
+                        new City() { Name = "Bucaramanga" },
+                        new City() { Name = "Málaga" },
+                        new City() { Name = "Barrancabermeja" },
+                        new City() { Name = "Rionegro" },
+                        new City() { Name = "Barichara" },
+                        new City() { Name = "Zapatoca" },
+                    }
+                },
+            }
                 });
                 _context.Countries.Add(new Country
                 {
                     Name = "Estados Unidos",
                     States = new List<State>()
-                    {
-                        new State()
-                        {
-                            Name = "Florida",
-                            Cities = new List<City>() {
-                                new City() { Name = "Orlando" },
-                                new City() { Name = "Miami" },
-                                new City() { Name = "Tampa" },
-                                new City() { Name = "Fort Lauderdale" },
-                                new City() { Name = "Key West" },
-                            }
-                        },
-                        new State()
-                        {
-                            Name = "Texas",
-                            Cities = new List<City>() {
-                                new City() { Name = "Houston" },
-                                new City() { Name = "San Antonio" },
-                                new City() { Name = "Dallas" },
-                                new City() { Name = "Austin" },
-                                new City() { Name = "El Paso" },
-                            }
-                        },
+            {
+                new State()
+                {
+                    Name = "Florida",
+                    Cities = new List<City>() {
+                        new City() { Name = "Orlando" },
+                        new City() { Name = "Miami" },
+                        new City() { Name = "Tampa" },
+                        new City() { Name = "Fort Lauderdale" },
+                        new City() { Name = "Key West" },
                     }
+                },
+                new State()
+                {
+                    Name = "Texas",
+                    Cities = new List<City>() {
+                        new City() { Name = "Houston" },
+                        new City() { Name = "San Antonio" },
+                        new City() { Name = "Dallas" },
+                        new City() { Name = "Austin" },
+                        new City() { Name = "El Paso" },
+                    }
+                },
+                new State()
+                {
+                    Name = "California",
+                    Cities = new List<City>() {
+                        new City() { Name = "Los Angeles" },
+                        new City() { Name = "San Francisco" },
+                        new City() { Name = "San Diego" },
+                        new City() { Name = "San Bruno" },
+                        new City() { Name = "Sacramento" },
+                        new City() { Name = "Fresno" },
+                    }
+                },
+            }
+                });
+                _context.Countries.Add(new Country
+                {
+                    Name = "Ecuador",
+                    States = new List<State>()
+            {
+                new State()
+                {
+                    Name = "Pichincha",
+                    Cities = new List<City>() {
+                        new City() { Name = "Quito" },
+                    }
+                },
+                new State()
+                {
+                    Name = "Esmeraldas",
+                    Cities = new List<City>() {
+                        new City() { Name = "Esmeraldas" },
+                    }
+                },
+            }
                 });
             }
 
             await _context.SaveChangesAsync();
         }
+
         private async Task CkeckCategoriesAsync()
         {
             if (!_context.Categories.Any())
