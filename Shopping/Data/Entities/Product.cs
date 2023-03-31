b92d7353a271e856d4c492c6246ed052952bc0fb
gallegosjennifer <jgallegos@myrobal.com>
2023-03-31 12:06:54 -0600

    Finish cart

--- Shopping/Data/Entities/Product.cs
+++ Shopping/Data/Entities/Product.cs
@@ -44,5 +44,7 @@ namespace Shopping.Data.Entities
         public string ImageFullPath => ProductImages == null || ProductImages.Count == 0
             ? $"https://localhost:7028/images/noimage.png"
             : ProductImages.FirstOrDefault().ImageFullPath;
+        public ICollection<SaleDetail> SaleDetails { get; set; }
+
     }
 }
